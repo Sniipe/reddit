712065f44ff156b9de5d7506cc11ec77ef194e06
Sniipe <rory.carroll@gmail.com>
2014-11-06 14:47:38 +0000

    Initial

---
+++ ConsoleApplication3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Dynamic;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic newUser = new ExpandoObject();
+            Console.WriteLine("Name>");
+            newUser.Name = Console.ReadLine();
+            Console.WriteLine("Age>");
+            newUser.Age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Username>");
+            newUser.Username = Console.ReadLine();
+
+            Console.WriteLine("Your name is " + newUser.Name + ", you are " + newUser.Age + " years old.  Your username is " + newUser.Username );            
+            Console.ReadKey();            
+        }
+    }
+}
